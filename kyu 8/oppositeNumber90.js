2d3167caf182b6ae865bf47243cb1b514f596f70
duarty <joseduartent@gmail.com>
2022-03-03 16:24:24 -0400

    Opposite number

---
+++ kyu 8/oppositeNumber90.js
@@ -0,0 +1,10 @@
+/* kata - https://www.codewars.com/kata/56dec885c54a926dcd001095
+José Duarte https://github.com/duarty - /joseduartent@gmail.com /whatsapp - +55 95 98100 1283 
+Very simple, given an integer or a floating-point number, find its opposite.
+
+*/
+
+function opposite(number) {
+    return number > 0 ? number * -1 : number * -1; //this function is simple, return the opposite number if input is -2, return 2 (positive)
+  }
+  //console.log(opposite(-2)) //line for tests
