af9c125051482aa737e33755eb3ed186c0b9b989
duarty <joseduartent@gmail.com>
2022-03-03 22:45:12 -0400

    Square(n) Sum

--- kyu 8/square(n)Sum.js
+++ kyu 8/square(n)Sum.js
@@ -1,4 +1,5 @@
 /* katas - https://www.codewars.com/kata/515e271a311df0350d00000f
+José Duarte https://github.com/duarty - /joseduartent@gmail.com /whatsapp - +55 95 98100 1283 
 Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
