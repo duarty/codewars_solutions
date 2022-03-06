302eaa981e1a27ac0c4d8eb28ff13ffcea435b26
duarty <joseduartent@gmail.com>
2022-03-05 20:54:09 -0400

    You Can't Code Under Pressure #1

---
+++ kyu 8/codeUnderPressure#1.js
@@ -0,0 +1,9 @@
+/* kata - https://www.codewars.com/kata/53ee5429ba190077850011d4
+José Duarte https://github.com/duarty - /joseduartent@gmail.com /whatsapp - +55 95 98100 1283 
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+    return i * 2;
+  }
+  You Can't Code Under Pressure #1
