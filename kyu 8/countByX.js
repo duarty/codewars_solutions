93e4ee68d08445180db4be0a8faf6652f0b04f7d
Jose Duarte <joseduartent@gmail.com>
2022-03-08 22:36:27 -0400

    countBy

--- kyu 8/countByX.js
+++ kyu 8/countByX.js
@@ -9,10 +9,10 @@ Examples:
 countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 */
-function countBy(x, n) {
-    let z = [];
-    for (let i = 1; i <= n; i++){
-        z.push(x * i)
+function countBy(x, n) { //mmultiples of x to n
+    let z = []; // create empty z array to storage the multiples
+    for (let i = 1; i <= n; i++){// for to count i = 0 to n, increment i in 1 by 1
+        z.push(x * i) // push the multiples in the array
     }
     return z;
 }
