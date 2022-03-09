1622d7ef5c13ec3f20277094e3d3c3546c784f81
Jose Duarte <joseduartent@gmail.com>
2022-03-08 22:23:15 -0400

    double integer

--- kyu 8/codeUnderPressure#1.js
+++ kyu 8/codeUnderPressure#1.js
@@ -4,6 +4,6 @@ Code as fast as you can! You need to double the integer and return it.
 */
 
 function doubleInteger(i) {
-    return i * 2;
+    return i * 2; //simple double inserted parameter
   }
   
